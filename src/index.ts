5e10ca0c39c947df8bf6055f065754be18a37d2d
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-08-24 19:41:45 +0800

    allow to make crud using mikeoORM typgraphql crud

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
-import {MikroORM} from '@mikro-orm/core'
-import {Post} from "./entities/Post";
+import 'reflect-metadata';
 import microConfig from './mikro-orm.config'
+import express from 'express';
+import {ApolloServer} from 'apollo-server-express'
+import {buildSchema} from "type-graphql";
+import {HelloResolver} from "./resolvers/hello";
+import {PostResolver} from "./resolvers/post";
+import {MikroORM} from "@mikro-orm/core";
 
 const main = async () => {
+    // @ts-ignore
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
-   /* const post = orm.em.create(Post, {title: 'my first title'});
-    await orm.em.persistAndFlush(post);*/
-    /*const posts = await orm.em.find(Post, {});
-    console.log(posts);*/
-}
 
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false
+        }),
+        context: () => ({em: orm.em})
+    });
+
+    apolloServer.applyMiddleware({app})
+
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000')
+    })
+}
 main().catch((err) => {
     console.error(err)
 });
---
+++ src/types.ts
@@ -0,0 +1,6 @@
+// @ts-ignore
+import {Connection, EntityManager, IDatabaseDriver} from "@mikro-orm/core";
+
+export type MyContext = {
+    em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
+};
